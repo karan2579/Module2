9e71fa0b2db021fe126020eb91bbf221560b1bf5
Abhigyan Pushkar <149153539+Abhiii1234@users.noreply.github.com>
2024-06-08 02:59:01 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,18 +1,20 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
-
-//import "hardhat/console.sol";
+pragma solidity ^0.8.16;
 
 contract Assessment {
     address payable public owner;
     uint256 public balance;
+    uint256 public Order;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
+    
+
 
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
+        Order = 0;
     }
 
     function getBalance() public view returns(uint256){
@@ -22,16 +24,11 @@ contract Assessment {
     function deposit(uint256 _amount) public payable {
         uint _previousBalance = balance;
 
-        // make sure this is the owner
         require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
         balance += _amount;
 
-        // assert transaction completed successfully
         assert(balance == _previousBalance + _amount);
 
-        // emit the event
         emit Deposit(_amount);
     }
 
@@ -48,7 +45,6 @@ contract Assessment {
             });
         }
 
-        // withdraw the given amount
         balance -= _withdrawAmount;
 
         // assert the balance is correct
@@ -57,4 +53,13 @@ contract Assessment {
         // emit the event
         emit Withdraw(_withdrawAmount);
     }
+    
+    function increment() public {
+        Order++;
+    }
+
+    function decrement() public {
+        Order--;
+    }
+    
 }
